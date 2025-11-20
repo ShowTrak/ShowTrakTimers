8c28345283f4b7f98c64f79d6ac923d496743226
Thomas Kirkman-Wood <tom@tkw.bz>
2025-11-20 19:43:37 +0000

    PWA Integration

---
+++ src/Modules/WebDashboard/public/service-worker.js
@@ -0,0 +1,56 @@
+const STATIC_CACHE = 'showtrak-dashboard-static-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/favicon.ico',
+  '/ui/img/icon.png',
+  '/ui/vendors/bootstrap/css/bootstrap.min.css',
+  '/ui/vendors/bootstrap-icons/font/bootstrap-icons.css',
+  '/ui/vendors/toastify/index.min.css',
+  '/ui/css/main.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(STATIC_CACHE).then((cache) => cache.addAll(STATIC_ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== STATIC_CACHE).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  if (request.method !== 'GET') return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request).catch(() => caches.match('/index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      const fetchPromise = fetch(request)
+        .then((networkResponse) => {
+          if (networkResponse && networkResponse.ok && networkResponse.type === 'basic') {
+            const cloned = networkResponse.clone();
+            caches.open(STATIC_CACHE).then((cache) => cache.put(request, cloned)).catch(() => {});
+          }
+          return networkResponse;
+        })
+        .catch(() => cachedResponse);
+
+      return cachedResponse || fetchPromise;
+    })
+  );
+});
